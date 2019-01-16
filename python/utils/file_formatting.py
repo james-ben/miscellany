db31785ce4569153a8dcc58830b86faba34a5d54
Benjamin James <bjames@byu.net>
2019-01-16 13:18:44 -0700

    Updates to give the PDF formatter more options

--- python/utils/file_formatting.py
+++ python/utils/file_formatting.py
@@ -11,10 +11,16 @@ except ImportError as e:
 	print("sudo apt install python3-tk")
 	sys.exit(1)
 
+# global variables
 filelist = []
 listbox = None
 root = None
 tabSize = 4
+output_filename = "formatted_code.pdf"
+outfile_box = None
+language = "C"
+font = "Courier8"
+fmt = "-R"
 
 
 def sortFileList():
@@ -32,16 +38,24 @@ def sortFileList():
 
 
 # runs the formatting script
+# enscript arguments:
+#	--color
+#	--tabsize: number of spaces in a tab
+#	-C: each line starts with line-number
+#	-B: no header
+#	-M: nedia output type
+#	-E: language syntax highlighting
+#	-f: font & size
+#	-R: portrait (-r is landscape)
 def runFormatter():
-	global filelist
+	global filelist, outfile_box, language, font, fmt
 	if len(filelist) > 0:
-		outfile = "test_formatting.pdf"
 		files = sortFileList()
 		allStrings = ' '.join(files)
 		# this is a hacky way to do it
-		f_cmd = "echo {} |  xargs enscript --color=1 --tabsize={} -C -B -M Letter -Ec -fCourier8 -o - | ps2pdf - {}".format(allStrings, tabSize, )
+		f_cmd = "echo {fls} |  xargs enscript --color=1 --tabsize={tabs} -C -B -M Letter -E{lang} -f{fnt} {align} -o - | ps2pdf - {of}".format(fls=allStrings, tabs=tabSize, align=fmt, of=outfile_box.get(), lang=language, fnt=font)
 		os.system(f_cmd)
-		print("Files have been formatted and written to {}".format(outfile))
+		print("Files have been formatted and written to {}".format(outfile_box.get()))
 		sys.exit(0)
 	else:
 		return
@@ -77,8 +91,26 @@ def delFiles():
 		del filelist[selection[0]]
 
 
+def choose_lang(value):
+	global language
+	language = value
+
+
+def choose_font(value):
+	global font
+	font = value
+
+
+def choose_orientation(value):
+	global fmt
+	if value == "portrait":
+		fmt = "-R"
+	else:
+		fmt = "-r"
+
+
 def main():
-	global listbox, root
+	global listbox, root, outfile_box
 
 	# check if the right packages are installed
 	#  this is specific to Ubuntu distributions, or others using APT package manager
@@ -95,12 +127,51 @@ def main():
 	# frames
 	top = tkinter.Frame(root)
 	top.pack(side=tkinter.TOP)
+	top2 = tkinter.Frame(root)
+	top2.pack(side=tkinter.TOP)
+	top3 = tkinter.Frame(root)
+	top3.pack(side=tkinter.TOP)
 	# bottom = tkinter.Frame(root)
 	# bottom.pack(side=tkinter.BOTTOM, fill=tkinter.BOTH, expand=True)
 
 	tkinter.Button(root, text="Add files", command=getSources).pack(in_=top, side=tkinter.LEFT)
 	tkinter.Button(root, text="Add folder", command=getDir).pack(in_=top, side=tkinter.LEFT)
 	tkinter.Button(root, text="Remove files", command=delFiles).pack(in_=top, side=tkinter.LEFT)
+
+	# drop down menus
+	l1 = tkinter.Label(root, text="Language:")
+	l1.pack(in_=top2, side=tkinter.LEFT)
+	# list all the possible styles with `enscript --help-highlight`
+	langs = ("c", "cpp", "html", "java", "javascript", "lua", "makefile", "matlab", "octave", "pascal", "perl", "postscript", "python", "sh", "tcl", "tex", "verilog", "vhdl")
+	dropVar1 = tkinter.StringVar()
+	dropVar1.set(langs[0]) # default choice
+	dropMenu1 = tkinter.OptionMenu(top2, dropVar1, *langs, command=choose_lang)
+	# dropMenu1.grid(column=1,row=4)
+	dropMenu1.pack(in_=top2, side=tkinter.LEFT)
+
+	l2 = tkinter.Label(root, text="Font:")
+	l2.pack(in_=top2, side=tkinter.LEFT)
+	fonts = ("Courier8", "Times-Roman12")
+	dropVar2 = tkinter.StringVar()
+	dropVar2.set(fonts[0])
+	dropMenu2 = tkinter.OptionMenu(top2, dropVar2, *fonts, command=choose_font)
+	dropMenu2.pack(in_=top2, side=tkinter.LEFT)
+
+	# orientation
+	l3 = tkinter.Label(root, text="orientation:")
+	l3.pack(in_=top2, side=tkinter.LEFT)
+	ors = ("portrait", "landscape")
+	dropVar3 = tkinter.StringVar()
+	dropVar3.set(ors[0])
+	dropMenu3 = tkinter.OptionMenu(top2, dropVar3, *ors, command=choose_orientation)
+	dropMenu3.pack(in_=top2, side=tkinter.LEFT)
+
+	# name of the output file
+	l3 = tkinter.Label(root, text="Output filename:")
+	l3.pack(in_=top3, side=tkinter.LEFT)
+	outfile_box = tkinter.Entry(root)
+	outfile_box.insert(0, "formatted_code.pdf")
+	outfile_box.pack(in_=top3, side=tkinter.LEFT)
 
 	# this displays all the files you picked
 	listbox = tkinter.Listbox(root)
